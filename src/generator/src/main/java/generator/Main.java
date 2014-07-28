2222ee9b35c814d4bbfede290f4c13659fc4b80d
Norberto <norberto@10gen.com>
2014-07-29 00:16:20 +0200

    setting up the command line parse

--- src/generator/src/main/java/generator/Main.java
+++ src/generator/src/main/java/generator/Main.java
@@ -6,6 +6,8 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.cli.*;
+
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
@@ -13,40 +15,57 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+
+        Options options = new Options();
+        Option hostOp = new Option("h", "host", true, "MongoDB host name");
+        Option threadsOp = new Option("t", "threads", true, "number of threads to be launched");
+        Option databaseOp = new Option("d", "database", true, "Database name");
+        Option collectionOp = new Option("c", "collection", true, "Collection name");
+        options.addOption(hostOp);
+        options.addOption(threadsOp);
+        options.addOption(databaseOp);
+        options.addOption(collectionOp);
         
-        
-       //number of threads
-       //mongodb uri
-       //
-       String host = "localhost";
-       String dbname = "dvla1";
-       String colname = "gendrivers";
-       MongoClient mc;
-       DBCollection collection= null;
-    try {
-        mc = new MongoClient(host);
-        collection = mc.getDB(dbname).getCollection(colname);
-    } catch (UnknownHostException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    }
-       
-       DocumentFactory factory = new DVLADriversFactory();
-       
-       int idSeed = 10000;
-       
-       BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(2);
-       ThreadPoolExecutor tpe = new ThreadPoolExecutor(10, 20, 1, TimeUnit.MILLISECONDS,queue);        
-       
-       queue.add(new Loader(collection, factory, idSeed));
-       queue.add(new Loader(collection, factory, idSeed+9999999));
-//       queue.add(new Loader(collection, factory, idSeed+19999999));
-       tpe.prestartAllCoreThreads();
-       tpe.execute( new MonitorThread(tpe, 1) );
-       
-//       while(true){
-//           
-//       }
+        String host = "localhost";
+        String dbname = "dvla1";
+        String colname = "gendrivers";
+        MongoClient mc;
+        DBCollection collection = null;
+        CommandLineParser parser = new BasicParser();
+        int nthreads = 1;
+        try {
+            CommandLine cmdLine = parser.parse(options, args);
+            if (cmdLine.hasOption('t'))
+                nthreads = Integer.valueOf(cmdLine.getOptionValue('t'));
+            if (cmdLine.hasOption('d'))
+                dbname = cmdLine.getOptionValue('d');
+            if (cmdLine.hasOption('c'))
+                colname = cmdLine.getOptionValue('c');
+            if (cmdLine.hasOption('h'))
+                host = cmdLine.getOptionValue('h');
+            
+            mc = new MongoClient(host);
+            collection = mc.getDB(dbname).getCollection(colname);
+
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ParseException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        DocumentFactory factory = new DVLADriversFactory();
+
+        int idSeed = 10000;
+
+        BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(nthreads);
+        ThreadPoolExecutor tpe = new ThreadPoolExecutor(10, 20, 1, TimeUnit.MILLISECONDS, queue);
+
+        for (int i = 0; i < nthreads; i++) {
+            queue.add(new Loader(collection, factory, idSeed));
+        }
+        tpe.prestartAllCoreThreads();
+        tpe.execute(new MonitorThread(tpe, 1));
 
     }
 
